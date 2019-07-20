a6a49aa6c73c3c7c48b70fc6a1757c01fd7da503
samanthajowilson <36824168+samanthajowilson@users.noreply.github.com>
2019-07-20 09:19:48 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,4 @@
+write("My Division Calculator!");
+var num1 = promptNum("Enter the first number to divide");
+var num2 = promptNum("Enter in the second number to divide by");
+write("Answer: "+num1 / num2);
